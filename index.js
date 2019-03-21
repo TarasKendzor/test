a096cc9fa79e3a04e88098215c9fb117deceb90f
TarasKendzor <45481754+TarasKendzor@users.noreply.github.com>
2019-03-21 18:34:21 +0200

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+$(window).on('load', function () {
+    var $preloader = $('#preloader'),
+        $svg_anm   = $preloader.find('.seq-preloade');
+    $svg_anm.fadeOut();
+    // $header = $('header').fadeOut('hide')
+    $preloader.delay(2000).fadeOut('slow');
+});
+// $(window).on('load', function () {
+//     $preloader = $('#preloader'),
+//       $loader = $preloader.find('.prePreloader');
+//     $loader.fadeOut();
+//     $preloader.delay(350).fadeOut('slow');
+//   });
+$('.nav-link').click(function(e){
+    e.preventDefault();
+    let menuScroll = e.target.id;
+    let thisBlock = '.'+ menuScroll;
+    $('html, body').animate({
+        scrollTop: $(thisBlock).offset().top
+    },700)
+})
+  $('#home-link').click(function(e){
+     $('html, body').animate({scrollTop: 0}, 500);
+        e.preventDefault();
+  })
+  $('#home').click(function(e){
+    $('html, body').animate({scrollTop: 0}, 500);
+       e.preventDefault();
+ })
+
